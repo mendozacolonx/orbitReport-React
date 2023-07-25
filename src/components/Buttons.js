b14ea04dd338e46aa99bfd8193240f1da97b5cc9
xiomaramendoza <mendozacolonx@gmail.com>
2023-07-25 14:02:47 -0500

    progress up to 7.25.23, 5 fails, 4 passes

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,24 @@
+import satData from "./satData.js";
+import filterByType from "../App.js";
+import setSat from "../App.js";
+import displaySats from "../App.js";
+import "./styling.css";
+
+
 const Buttons = () => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, id) => {
+        return (
+          <button className = {flex-container} onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => setSat(satData)}>
+        All Orbits
+      </button>
+    </div>
   );
 };
 
